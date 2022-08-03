ec3e8823e37287f49cd563cfb1cc736d98bd418a
todak-sun <tjsdydwn@gmail.com>
2022-08-03 22:09:54 +0900

    차후 RabbitMQ는 Config와 Client 분리하자

--- scrapper/src/message/message.spec.ts
+++ scrapper/src/message/message.spec.ts
@@ -1,30 +1,14 @@
-import { Channel } from 'amqplib';
 import { Configurator } from '../core/configurator';
 import { RabbitMQPublisher } from './message';
 describe(`RabbitMQPublisher`, () => {
-  it(`getConnection()`, async () => {
+  it(`publish()`, async () => {
     const publisher = new RabbitMQPublisher(new Configurator());
-    const connection = await publisher.getConnection();
+    await publisher.connect({ topicName: `CONVINIENCE.EVENT.GOODS`, routingKey: `ITEM`, queueNames: [`PROCESSOR`] });
 
-    const channel: Channel = await connection.createChannel();
-    const routingKey = `ITEM`;
-    const topicName = `CONVINIENCE.EVENT.GOODS`;
-    await channel.assertExchange(topicName, `topic`, { durable: false });
+    for (let i = 0; i < 10; i++) {
+      await publisher.publish({ hello: `world${i}` });
+    }
 
-    await Promise.all(
-      [`PROCESSOR`]
-        .map((queueName) => [topicName, queueName].join('.'))
-        .map(async (queueName) => {
-          await channel.assertQueue(queueName, { durable: true });
-          await channel.bindQueue(queueName, topicName, routingKey);
-        })
-    );
-
-    channel.publish(topicName, routingKey, Buffer.from(JSON.stringify({ hello: `world` })), {
-      contentType: `application/json`,
-      contentEncoding: `utf-8`,
-    });
-    await channel.close()
-    await connection.close();
+    await publisher.close();
   });
 });
--- scrapper/src/message/message.ts
+++ scrapper/src/message/message.ts
@@ -1,20 +1,79 @@
-import { connect, Connection } from 'amqplib';
+import { Channel, connect, Connection } from 'amqplib';
 import { Configurator } from '../core/configurator';
+
+export type RabbitMQPublisherConnectOptions = {
+  topicName: string;
+  routingKey: string;
+  queueNames?: string[];
+};
 
 export class RabbitMQPublisher {
   private connection: Connection | undefined;
+
+  private topicName: string | undefined;
+  private queueNames: string[] | undefined;
+  private routingKey: string | undefined;
+
   constructor(private readonly configurator: Configurator) {}
 
-  getConnection = async () => {
-    if (!this.connection) {
-      this.connection = await connect({
-        hostname: this.configurator.get(`RABBITMQ_HOSTNAME`),
-        protocol: this.configurator.get(`RABBITMQ_PROTOCOL`),
-        port: this.configurator.getNumber(`RABBITMQ_PORT`),
-      });
+  connect = async (options: RabbitMQPublisherConnectOptions) => {
+    if (this.connection) {
+      throw new Error(`이미 연결중입니다.`);
     }
-    return this.connection;
+    const { topicName, queueNames, routingKey } = options;
+    this.connection = await connect({
+      hostname: this.configurator.get(`RABBITMQ_HOSTNAME`),
+      protocol: this.configurator.get(`RABBITMQ_PROTOCOL`),
+      port: this.configurator.getNumber(`RABBITMQ_PORT`),
+    });
+
+    process.once(`SIGINT`, async () => this.connection?.close());
+
+    const channel: Channel = await this.connection.createChannel();
+    await channel.assertExchange(topicName, `topic`, { durable: true });
+    if (queueNames) {
+      await Promise.all(
+        queueNames
+          .map((queueName) => [topicName, queueName].join('.'))
+          .map(async (queueName) => {
+            await channel.assertQueue(queueName, { durable: true });
+            await channel.bindQueue(queueName, topicName, routingKey);
+          })
+      );
+    }
+    await channel.close();
+
+    this.topicName = topicName;
+    this.routingKey = routingKey;
+    if (queueNames) {
+      this.queueNames = queueNames;
+    }
   };
 
-  close = async () => await this.connection?.close();
+  publish = async (message: any) => {
+    if (!this.connection) {
+      throw new Error(`현재 연결되어 있지 않습니다.`);
+    }
+    const ch: Channel = await this.connection.createChannel();
+    this.publishInternal(ch, message);
+    await ch.close();
+  };
+
+  publishAll = async (messages: any[]) => {
+    if (!this.connection) {
+      throw new Error(`현재 연결되어 있지 않습니다.`);
+    }
+    const ch: Channel = await this.connection.createChannel();
+    messages.forEach((message) => this.publishInternal(ch, message));
+    await ch.close();
+  };
+
+  private publishInternal = (ch: Channel, message: any): boolean => {
+    return ch.publish(this.topicName!, this.routingKey!, Buffer.from(JSON.stringify(message)), {
+      contentEncoding: `utf-8`,
+      contentType: `application/json`,
+    });
+  };
+
+  close = async (): Promise<void> => await this.connection?.close();
 }
